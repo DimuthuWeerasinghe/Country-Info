36aeb7fcf6f946ea00c88e3aec3656fc35b13354
Your Name <you@example.com>
2024-09-11 11:25:14 +0530

    second

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 fetch("https://restcountries.com/v3.1/all")
 .then(res=>res.json())
-.then(data=>console.log("Fuck"))
+.then(data=>{
+    let tblCountries=document.getElementById("tbl");
+    
+    let tblBody= `<tr>
+        <th>Name</th>
+        <th>Flag</th>
+    </tr>`
+    data.forEach(element => {
+        tblBody+=`<tr>
+        <td>${element.name.common}</td>
+        <td><img src="${element.flags.png}"></td>
+    </tr>`
+        console.log(element.name.common);
+    });
+    tblCountries.innerHTML=tblBody;
+})
